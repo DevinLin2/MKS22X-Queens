9d0bec23715f803b1824e1c7639269e758f82d82
VitaminOh <dlin01@stuy.edu>
2019-02-08 09:42:40 -0500

    fixed add and remove queen functions; finished tostring and tested

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,16 +1,30 @@
 public class QueenBoard {
 
   private int[][] board;
-
+  public static void main(String[] args) {
+    QueenBoard board = new QueenBoard(8);
+    System.out.println(board);
+    board.addQueen(0,0);
+    board.addQueen(1,7);
+    System.out.println(board);
+  }
   public QueenBoard(int size) {
     board = new int[size][size];
   }
 
   private boolean addQueen(int row, int col) {
-    board[row][col] = -1;
+    if (board[row][col] != -1 && row < board.length && col < board[row].length) {
+      board[row][col] = -1;
+      return true;
+    }
+    return false;
   }
   private boolean removeQueen(int row, int col) {
-    board[row][col] = 0;
+    if (board[row][col] == -1 && row < board.length && col < board[row].length) {
+      board[row][col] = 0;
+      return true;
+    }
+    return false;
   }
   /**
   *@return The output string formatted as follows:
@@ -30,13 +44,15 @@ public class QueenBoard {
     String ans = "";
     for (int row = 0; row < board.length; row ++) {
       for (int col = 0; col < board[row].length; col ++) {
-        if (board[row][col] = -1) {
-          ans += "Q";
+        if (board[row][col] == -1) {
+          ans += "Q ";
         } else {
-          ans += "_";
+          ans += "_ ";
         }
       }
+      ans += "\n";
     }
+    return ans;
   }
   /**
   *@return false when the board is not solveable and leaves the board filled with zeros;
@@ -44,13 +60,13 @@ public class QueenBoard {
   *@throws IllegalStateException when the board starts with any non-zero value
   */
   public boolean solve() {
-
+    return true;
   }
   /**
   *@return the number of solutions found, and leaves the board filled with only 0's
   *@throws IllegalStateException when the board starts with any non-zero value
   */
   public int countSolutions() {
-
+    return 0;
   }
 }
